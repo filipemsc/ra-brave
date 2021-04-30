bc685984d0b52f147047939eb87594a54e2af3c8
filipemsc <f.matheus.cavalcanti@gmail.com>
2021-04-30 18:13:10 -0300

    ajeita go

--- input/r/GO_2018-2020.R
+++ input/r/GO_2018-2020.R
@@ -25,13 +25,9 @@ GO_Tratado = GO_2018_2020 %>%
                                                                            id = "Latin-ASCII") %>%
                                     stringr::str_to_lower()) %>%
               select(municipio2, estado_abrev, id_municipio, id_estado),
-<<<<<<< HEAD
             by=c("state"="estado_abrev", "city"="municipio2")) %>% 
-  relocate(id_ocorr, id_estado, state, id_municipio, city, neighbour, month, day, year, crime, 
+  relocate(id_estado, state, id_municipio, city, neighbour, month, day, year, crime, 
            sex_victim, age_victim, race_victim, school_victim, motivation)
-=======
-            by=c("state"="estado_abrev", "city"="municipio2"))
->>>>>>> main
 
 
 saveRDS(GO_Tratado, "input/clean/T_GO_2018-2020.rds")
