bb13334fa7207d08e982fae1152804337dce86fa
filipemsc <f.matheus.cavalcanti@gmail.com>
2021-04-30 17:18:27 -0300

    a

--- input/r/GO_2018-2020.R
+++ input/r/GO_2018-2020.R
@@ -7,8 +7,7 @@ municipios = read_csv("input/diretorio_municipios.csv")
 
 GO_Tratado = GO_2018_2020 %>%
 
-  transmute(id_ocorr = NA,
-    city = stringr::str_to_lower(MUNICÍPIO) %>% stringi::stri_trans_general(str = ., 
+  transmute(city = stringr::str_to_lower(MUNICÍPIO) %>% stringi::stri_trans_general(str = ., 
                                                                                   id = "Latin-ASCII"),
             year = ANO,
             month = MÊS,
@@ -26,9 +25,13 @@ GO_Tratado = GO_2018_2020 %>%
                                                                            id = "Latin-ASCII") %>%
                                     stringr::str_to_lower()) %>%
               select(municipio2, estado_abrev, id_municipio, id_estado),
+<<<<<<< HEAD
             by=c("state"="estado_abrev", "city"="municipio2")) %>% 
   relocate(id_ocorr, id_estado, state, id_municipio, city, neighbour, month, day, year, crime, 
            sex_victim, age_victim, race_victim, school_victim, motivation)
+=======
+            by=c("state"="estado_abrev", "city"="municipio2"))
+>>>>>>> main
 
 
 saveRDS(GO_Tratado, "input/clean/T_GO_2018-2020.rds")
