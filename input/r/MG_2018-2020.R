7b93a7e54b00b93e10d7f2b4d0100be39b6562ca
Pedro Feijó <pedro_feijo@usp.br>
2021-05-14 13:13:12 -0300

    Adiciona MG

--- input/r/MG_2018-2020.R
+++ input/r/MG_2018-2020.R
@@ -2,19 +2,20 @@ library(readxl)
 library(dplyr)
 library(readr)
 
-# MUDEM O CAMINHO DO DESKTOP!
 
-MG_2018_2020 = readxl::read_excel("C:/Users/pedro/Desktop/Pedro Feijó/FEA-USP/2021/BWE - EConomistas/BRAVE/Violencia Domestica/Bases_NT/MG_2018-2020.xlsx")
-municipios = read_csv("C:/Users/pedro/Desktop/Pedro Feijó/FEA-USP/2021/BWE - EConomistas/BRAVE/Violencia Domestica/diretorio_municipios.csv")
+
+MG_2018_2020 = readxl::read_excel("input/raw/MG_2018-2020.xlsx")
+municipios = read_csv("input/diretorio_municipios.csv")
 
 MG_Tratado = MG_2018_2020 %>%
   
-  transmute(city = stringr::str_to_lower(MUNICIPIO) %>% stringi::stri_trans_general(str = ., 
+  transmute(id_ocorr = NA,
+    city = stringr::str_to_lower(MUNICIPIO) %>% stringi::stri_trans_general(str = ., 
                                                                                     id = "Latin-ASCII"),
             
             
             year = ANO,
-            month = MÊS,
+            month = MÃŠS,
             day = DIA,
             neighbour = stringr::str_to_lower(SUBLOCALIZACAO),
             state = "MG",
@@ -25,13 +26,30 @@ MG_Tratado = MG_2018_2020 %>%
             school_victim = NA, 
             motivation = MOTIVACAO) %>%
   
+  mutate(city = gsub("abre-campo", "abre campo",city),
+         city = gsub("pingo d'agua", "pingo-d'agua",city),
+         city = gsub("brasopolis", "brazopolis",city), 
+         city = gsub("entre-rios de minas", "entre rios de minas",city), 
+         city = gsub("grao-mogol", "grao mogol",city), 
+         city = gsub("sao sebast. do maranhao", "sao sebastiao do maranhao",city), 
+         city = gsub("sao sebastiao paraiso", "sao sebastiao do paraiso",city), 
+         city = gsub("estrela-dalva", "estrela dalva",city),
+         city = gsub("capitao eneias", "capitao eneas",city),
+         city = gsub("passa vinte", "passa-vinte",city),
+         city = gsub("nao informado", NA ,city),
+         city = gsub("dores de ganhaes", "dores de guanhaes",city))%>%
+  
   left_join(municipios %>% mutate(municipio2 = stringi::stri_trans_general(str = municipio, 
                                                                            id = "Latin-ASCII") %>%
                                     stringr::str_to_lower()) %>%
               select(municipio2, estado_abrev, id_municipio, id_estado),
-            by=c("state"="estado_abrev", "city"="municipio2"))
+            by=c("state"="estado_abrev", "city"="municipio2"))%>%
+  
+  relocate(id_ocorr, id_estado, state, id_municipio, city, neighbour, month, day, year, crime, 
+           sex_victim, age_victim, race_victim, school_victim, motivation)
 
 
-saveRDS(MG_Tratado, "C:/Users/pedro/Desktop/Pedro Feijó/FEA-USP/2021/BWE - Economistas/BRAVE/Violencia Domestica/Bases_T/T_MG_2018-2020.rds")
-# MUDEM O CAMINHO DO DESKTOP!
+
+saveRDS(MG_Tratado, "input/clean/T_MG_2018-2020.rds")
+
 
